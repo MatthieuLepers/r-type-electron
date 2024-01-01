dd059fdc2e180358c7c5bc864151785c0688d049
Matthieu LEPERS <aireayquaza@hotmail.fr>
2024-01-01 16:15:55 +0100

    :fix: style patheditor

--- src/renderer/views/Menu/Tools/PathEditor.vue
+++ src/renderer/views/Menu/Tools/PathEditor.vue
@@ -1,12 +1,26 @@
 <template>
   <MainLayout class="path-editor-screen">
-    <PathEditor />
+    <Screen>
+      <template v-slot:content>
+        <PathEditor />
+      </template>
+      <template v-slot:footer>
+        <ScreenMenuButton
+          :to="{ name: 'MenuTools' }"
+          :modifiers="{ tiny: true }"
+        >
+          Back
+        </ScreenMenuButton>
+      </template>
+    </Screen>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from '@renderer/views/MainLayout.vue';
 import PathEditor from '@renderer/components/PathEditor/index.vue';
+import Screen from '@renderer/components/Screen/index.vue';
+import ScreenMenuButton from '@renderer/components/Screen/MenuButton.vue';
 
 defineOptions({ name: 'MenuPathEditorToolScreen' });
 </script>
